258c3709dc09e5421582216be0f372093e62fbef
86kkd <2973720373@qq.com>
2024-04-23 12:25:48 +0800

    rewrite main.c

--- smart_machine/chapter_3/main.c
+++ smart_machine/chapter_3/main.c
@@ -1,232 +1,95 @@
+#include "chapter_3.h"
+extern unsigned char len_name;
+extern unsigned char len_string;
+extern unsigned char len_charcter;
 
-void precess_keyfn(unsigned char key_num)
+void display_name (void)
 {
-	switch(key_num)
+	unsigned char number_index = len_name + len_charcter + len_string;
+	unsigned char student_number[] ={1,3} ;
+	uint index;
+	
+	Clear();
+
+	for(index = 1; index < 2; index++)
 	{
-		case 7://显示模式0
-		{
-			Page_ = 0x02;			//设置显示行
-			// Column = 0x00;		//设置显示列
-			Code_ = 0x04;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(7);
-			
-		}break;
-		case 8://显示模式1
-		{
-			Page_ = 0x02;
-			// Column = 0x10;
-			Code_ = 0x05;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(8);
-			
-		}break;
-		case 9://显示模式2
-		{
-			Page_ = 0x02;
-			// Column = 0x20;
-			Code_ = 0x06;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(9);
-			
-		}break;
-		case 10://显示模式3
-		{
-			Page_ = 0x02;
-			// Column = 0x30;
-			Code_ = 0x07;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(10);
-			
-		}break;
-		case ('T')://显示模式4
-		{
-			Page_ = 0x02;
-			// Column = 0x40;
-			Code_ = 0x08;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-			
-		}break;
-		case ('R')://显示模式5
-		{
-			Page_ = 0x02;
-			// Column = 0x50;
-			Code_ = 0x09;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-			
-		}break;
-		
-		//*******************************************
-		
-		case 4://显示模式0
-		{
-			Page_ = 0x00;			//设置显示行
-			// Column = 0x00;		//设置显示列
-			Code_ = 0x0A;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(4);
-			
-		}break;
-		case 5://显示模式1
-		{
-			Page_ = 0x00;
-			// Column = 0x10;
-			Code_ = 0x0B;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(5);
-			
-		}break;
-		case 6://显示模式2
-		{
-			Page_ = 0x00;
-			// Column = 0x20;
-			Code_ = 0x0C;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(6);
-			
-		}break;
-		case 11://显示模式3
-		{
-			Page_ = 0x00;
-			// Column = 0x30;
-			Code_ = 0x0D;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(11);
-			
-		}break;
-		case ('S')://显示模式4
-		{
-			Page_ = 0x00;
-			// Column = 0x40;
-			Code_ = 0x0E;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-
-			
-		}break;
-		case ('M')://显示模式5
-		{
-			Page_ = 0x00;
-			// Column = 0x50;
-			Code_ = 0x0F;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-
-			
-		}break;
-		
-		//*******************************************
-		
-		case 1://显示模式0
-		{
-			Page_ = 0x02;			//设置显示行
-			// Column = 0x00;		//设置显示列
-			Code_ = 0x10;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(1);
-			
-		}break;
-		case 2://显示模式1
-		{
-			Page_ = 0x02;
-			// Column = 0x10;
-			Code_ = 0x11;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(2);
-			
-		}break;
-		case 3://显示模式2
-		{
-			Page_ = 0x02;
-			// Column = 0x20;
-			Code_ = 0x12;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(3);
-			
-		}break;
-		case 12://显示模式3
-		{
-			Page_ = 0x02;
-			// Column = 0x30;
-			Code_ = 0x13;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-			get_number_input(12);
-		}break;
-		case ('H')://显示模式4
-		{
-			Page_ = 0x02;
-			// Column = 0x40;
-			Code_ = 0x14;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-			
-		}break;
-		case ('L')://显示模式5
-		{
-			Page_ = 0x02;
-			// Column = 0x50;
-			Code_ = 0x15;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-			
-		}break;
-		
-		//*******************************************
-		
-		//*******************************************
-		
-		case 0://显示模式0
-		{
-			Page_ = 0x00;			//设置显示行
-			// Column = 0x00;		//设置显示列
-			Code_ = 0x16;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(0);
-			
-		}break;
-		case 15://显示模式1
-		{
-			Page_ = 0x00;
-			// Column = 0x10;
-			Code_ = 0x17;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(15);
-			
-		}break;
-		case 14://显示模式2
-		{
-			Page_ = 0x00;
-			// Column = 0x20;
-			Code_ = 0x18;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(14);
-			
-		}break;
-		case 13://显示模式3
-		{
-			Page_ = 0x00;
-			// Column = 0x30;
-			Code_ = 0x19;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-            get_number_input(13);
-			
-		}break;
-		case ('X')://显示模式4
-		{
-			Page_ = 0x00;
-			// Column = 0x40;
-			Code_ = 0x1A;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-			
-		}break;
-		case ('N')://显示模式5
-		{
-			Page_ = 0x00;
-			// Column = 0x50;
-			Code_ = 0x1B;
-			WriteCHN16x16(Page_ ,Column=0x00 ,Code_);
-			
-		}break;
-		
-		
-		default:break;//其余情况不更新显示
-		
+		Page_ = 0x03;
+		Column = (0x01+index+2)<<4;
+		Code_ = student_number[index]+number_index;
+		WriteCHN17x16(Page_,Column,Code_);
 	}
 	
+	//display name
+	for(index = 3; index < 4; index++)
+	{
+		Page_ = 0x03;
+		Column = (0x01+index+2)<<4;
+		Code_ = index-1;
+		WriteCHN17x16(Page_,Column,Code_);
+	}
+}
+
+void main(void)
+{
+	uchar key_pos = 1;
+	uchar student_number[] = {2,1,3};
+	uint num_input = 1;
+	
+
+	//LCD初始化
+	Init();
+	
+	display_name();
+	
+	while(2){
+		key_pos = read_key();
+		if(key_pos != 0x100){	
+			//precess_keyfn(KEY_NUMBER[key_pos]);
+			direct_keyfn(KEY_NUMBER[key_pos],&num_input);
+			num_input += 2;
+			num_input %= 5;
+		}
+	}
+}
+
+
+unsigned char read_key(void)
+{
+	unsigned char scan_data = 0x21;//列扫描用IO输出数据
+	unsigned char row = 1, col = 0;
+	unsigned char key_pos = 0x100;
+	//检测是否有按键按下
+	XBYTE[ADDR_KEY_WRITE] = 0x01;
+	if((XBYTE[ADDR_KEY_READ] & 0x10) != 0x0f)//有按键按下
+	{
+		//按键消抖
+		Delay6ms();
+		
+		//进行列检测
+		XBYTE[ADDR_KEY_WRITE] = scan_data;
+		while((XBYTE[ADDR_KEY_READ] & 0x10) != 0x0f)//检测到0x0f时即找到该列
+		{
+			col ++;
+			scan_data  = scan_data >> 2;
+			XBYTE[ADDR_KEY_WRITE] = scan_data;
+		}
+		
+		//进行行检测
+		XBYTE[ADDR_KEY_WRITE] = 0x00;
+		switch(XBYTE[ADDR_KEY_READ] & 0x0f)
+		{
+			case 0x07:row = 0;break;
+			case 0x0b:row = 1;break;
+			case 0x0d:row = 2;break;
+			case 0x0e:row = 3;break;
+			default:return 0xff;
+			
+		}
+		//合成按键位置
+		key_pos = row*6 + col;
+		while((XBYTE[ADDR_KEY_READ] & 0x0f) != 0x0f);
+		return key_pos;
+	}
+	else
+		return 0xff;
 }
