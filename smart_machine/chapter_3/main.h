71ddd48d71381aac65f6342f50a0c75b7ab4d589
86kkd <2973720373@qq.com>
2024-04-23 11:18:06 +0800

    uchar change

--- smart_machine/chapter_3/main.h
+++ smart_machine/chapter_3/main.h
@@ -3,8 +3,9 @@
 #include "font_library.h"
 
 typedef unsigned char uchar;
+typedef unsigned int uint;
 
-unsigned char code KEY_NUMBER[]={
+uchar code KEY_NUMBER[]={
 
 7,	8,	9,	10,	'T',	'R',
 
@@ -16,39 +17,39 @@ unsigned char code KEY_NUMBER[]={
 };//按键键名
 
 //显示按键键名
-#define MODE_0	(unsigned char)0
-#define MODE_1	(unsigned char)1
-#define MODE_2	(unsigned char)2
-#define MODE_3	(unsigned char)3
+#define MODE_0	(uchar)0
+#define MODE_1	(uchar)1
+#define MODE_2	(uchar)2
+#define MODE_3	(uchar)3
 
 
 
 #define ADDR_KEY_READ	0x9001
 #define ADDR_KEY_WRITE	0x9002
 
-xdata unsigned char CWADD1 _at_ 0x8000;   // 写指令代码地址(E1)
-xdata unsigned char DWADD1 _at_ 0x8001;   // 写显示数据地址(E1)
-xdata unsigned char CRADD1 _at_ 0x8002;   // 读状态字地址(E1)
-xdata unsigned char DRADD1 _at_ 0x8003;   // 读显示数据地址(E1)
+xdata uchar CWADD1 _at_ 0x8000;   // 写指令代码地址(E1)
+xdata uchar DWADD1 _at_ 0x8001;   // 写显示数据地址(E1)
+xdata uchar CRADD1 _at_ 0x8002;   // 读状态字地址(E1)
+xdata uchar DRADD1 _at_ 0x8003;   // 读显示数据地址(E1)
 
-xdata unsigned char CWADD2 _at_ 0x8004;   // 写指令代码地址(E2)
-xdata unsigned char DWADD2 _at_ 0x8005;   // 写显示数进地址(E2)
-xdata unsigned char CRADD2 _at_ 0x8006;   // 读状态字地址(E2)
-xdata unsigned char DRADD2 _at_ 0x8007;   // 读显示数据地址(E2)
-
-
-unsigned char Column;
-unsigned char Page_ ;  // 页地址寄存器 D1,DO:页地址
-unsigned char Code_ ;  // 字符代码寄存器
-
-unsigned char Command; // 指令寄存器
-unsigned char LCDData; // 数据寄存器
+xdata uchar CWADD2 _at_ 0x8004;   // 写指令代码地址(E2)
+xdata uchar DWADD2 _at_ 0x8005;   // 写显示数进地址(E2)
+xdata uchar CRADD2 _at_ 0x8006;   // 读状态字地址(E2)
+xdata uchar DRADD2 _at_ 0x8007;   // 读显示数据地址(E2)
 
 
+uchar Column;
+uchar Page_ ;  // 页地址寄存器 D1,DO:页地址
+uchar Code_ ;  // 字符代码寄存器
 
-unsigned char read_key(void);										//按键扫描
+uchar Command; // 指令寄存器
+uchar LCDData; // 数据寄存器
+
+
+
+uchar read_key(void);										//按键扫描
 void Delay5ms();					// 延时程序											//软件延时
-void precess_keyfn(unsigned char key_num);	//LCD显示模式切换
+void precess_keyfn(uchar key_num);	//LCD显示模式切换
 				 
 
 void WriteCommandE1();		// 1.写指令代码子程序(E1)		 
